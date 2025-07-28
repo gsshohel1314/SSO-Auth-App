f85b9dec2fb5f03b179c89ff0bef3c1dc47e4df2
shohel <gsshohel1314@gmail.com>
2025-07-29 01:31:45 +0600

    Custom sign in and sign out functionality added

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserTableSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Website User',
+            'email' => 'website@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
